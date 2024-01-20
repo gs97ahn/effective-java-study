7755987e4762663351ee14b69685060018305028
gs97ahn <gs97ahn@naver.com>
2024-01-20 14:23:30 +0900

    add : [item33] 타입 안전 이종 컨테이너를 고려하라

---
+++ src/main/java/ahn/chapter5/item33/favorites/FavoritesTest.java
@@ -0,0 +1,21 @@
+package ahn.chapter5.item33.favorites;
+
+import org.junit.jupiter.api.Test;
+
+public class FavoritesTest {
+
+    @Test
+    void test1() {
+        Favorites f = new Favorites();
+
+        f.putFavorite(String.class, "Java");
+        f.putFavorite(Integer.class, 0xcafebabe);
+        f.putFavorite(Class.class, Favorites.class);
+
+        String favoriteString = f.getFavorite(String.class);
+        int favoriteInteger = f.getFavorite(Integer.class);
+        Class<?> favoriteClass = f.getFavorite(Class.class);
+
+        System.out.printf("%s %x %s%n", favoriteString, favoriteInteger, favoriteClass.getName());
+    }
+}
